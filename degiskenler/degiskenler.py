030d8c9659586aef4632ddd0a706aa1c066621f7
yeniturkalp <56562039+yeniturkalp@users.noreply.github.com>
2022-01-22 21:40:03 +0300

    Değişkenler
    Değişkenler ile ilgili kodlar yazıldı.

---
+++ degiskenler/degiskenler.py
@@ -0,0 +1,73 @@
+x = "Python"
+
+def ornekFonksiyon():
+    y = "Biyoinformatik"
+    print(x)
+    print(y)  # biyoinformatik
+
+ornekFonksiyon()
+
+print(x)  # python
+
+
+#Çoklu Değişken Değerleri Tanımlama
+
+x, y, z = "Elma", "Armut", "Çilek"
+
+print(x) #Elma 
+print(y) #Armut 
+print(z) #Çilek
+
+
+# Kullanıcıdan Değişkene Atanacak Değer Alma
+
+meslek = input("Mesleğiniz Nedir?")
+
+print(meslek) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
